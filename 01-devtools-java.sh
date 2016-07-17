80a0b97a68ecdf45cf6c40fbbe2e20ad524ebdc2
Leo Simons <lsimons@schubergphilis.com>
2016-07-17 20:17:47 +0200

    Cope with crazy silly tools.jar for old jdk on mac.

--- 01-devtools-java.sh
+++ 01-devtools-java.sh
@@ -15,10 +15,39 @@ brew cask install ./jce-unlimited-strength-policy7.rb
 brew cask install Caskroom/versions/java6
 brew cask install ./jce-unlimited-strength-policy6.rb
 
+ensure_tools_jar() {
+  local java_home=$1
+  owd=$PWD
+  cd $java_home/lib;
+  if [[ ! -f tools.jar ]]; then
+    if [[ -f ../../Classes/classes.jar ]]; then
+      sudo ln -s ../../Classes/classes.jar tools.jar
+    else
+      echo "WARNING: no tools.jar in $java_home and don't know where it is"
+    fi
+  fi
+
+  # maven expectations
+  cd $java_home/..
+  if [[ ! -d lib ]]; then
+    sudo mkdir lib
+  fi
+  if [[ ! -f lib/tools.jar ]]; then
+    if [[ -f $java_home/lib/tools.jar ]]; then
+      sudo ln -s $java_home/lib/tools.jar lib/tools.jar
+    else
+      echo "WARNING: no tools.jar in $java_home/../lib maven could freak"
+      echo "  see http://maven.apache.org/general.html#tools-jar-dependency"
+    fi
+  fi
+
+  cd $PWD
+}
 
 for v in 1.6 1.7 1.8; do
+  java_home=$(/usr/libexec/java_home -v $v)
+  ensure_tools_jar $java_home
   if [[ "$(jenv versions | grep $v)" == "" ]]; then
-    java_home=$(/usr/libexec/java_home -v $v)
     jenv add $java_home
   fi
 done
